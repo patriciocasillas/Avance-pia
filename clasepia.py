cd81b6745de5de5006b9a502274e5bd4310650e2
patriciocasillas <61504557+patriciocasillas@users.noreply.github.com>
2020-05-22 19:42:11 -0500

    Create clasepia.py

---
+++ clasepia.py
@@ -0,0 +1,10 @@
+import datetime
+import csv
+class Contacto :
+    def __init__( self,NICKNAME,NOMBRE,CORREO,TELEFONO,FECHANACIMIENTO,GASTO):
+        self.NICKNAME = NICKNAME
+        self.NOMBRE = NOMBRE
+        self.CORREO = CORREO
+        self.TELEFONO = TELEFONO
+       self.FECHANACIMIENTO = FECHANACIMIENTO
+        self.GASTO = GASTO
